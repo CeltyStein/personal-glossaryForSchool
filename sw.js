69346e8ba99a156a46855db5d784194504065b88
CeltyStein <113092568+CeltyStein@users.noreply.github.com>
2025-10-19 19:43:47 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='glossary-cache-v1';const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='glossary-cache-v2';
+const ASSETS=['./','./index.html','./cram.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})))})
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;})))});
